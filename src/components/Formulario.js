f9db24d2ecb15247375eb18f333c4b37962e56ca
Daniel Reina Ramos <drrmovil@gmail.com>
2021-07-24 12:32:02 +0200

    empezando la funcion de cotizar

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Error from './Error'
 import styled from "@emotion/styled";
 import useMoneda from "../hooks/useMoneda";
+import useCriptomoneda from "../hooks/useCriptomenda";
+import axios from 'axios'
 
 const Boton = styled.input`
   margin-top: 20px;
@@ -20,14 +23,56 @@ const Boton = styled.input`
   }
 `;
 
-const Formulario = () => {
-//Utilizar UseMoneda
-    const [moneda, SeletMonedas, actualizarState] = useMoneda(
-      
-    );
+  
 
-    return <form>
-        <SeletMonedas/>
+
+const Formulario = () => {
+  // state del listado de criptomonedas
+  const [listacripto, guardarCriptomonedas] = useState([])
+  const [error, guardarError]=useState(false)
+
+const MONEDAS = [
+    { codigo: 'USD', nombre: 'Dolar de Estados Unidos' },
+    { codigo: 'MXN', nombre: 'Peso Mexicano' },
+    { codigo: 'EUR', nombre: 'Euro' },
+    { codigo: 'GBP', nombre: 'Libra Esterlina' },
+  ]
+
+//Utilizar useMoneda
+    const [moneda, SelectMonedas, actualizarState] = useMoneda(
+      'Elige tu Moneda','',MONEDAS
+    );
+//Utilizar useCriptomoneda
+  const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda', '',listacripto)
+  
+//Ejecutar llamado a la Api
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+      const resultado = await axios.get(url)
+      guardarCriptomonedas(resultado.data.Data)
+    }
+    consultarAPI()
+},[])
+  
+  // cuando el usuario hace submit
+  const cotizarMoneda = e => {
+    e.preventDefault();
+    //validar si ambos campos estan llenos
+    if (moneda === '' || criptomoneda === '') {
+      guardarError(true)
+      return
+    }
+    //pasar los datos al componente principal
+    guardarError(false)
+  }
+  
+  return <form
+  onSubmit={cotizarMoneda}
+  >
+    {error?<Error mensaje='Todos los campos son obligatorios'/>:null}
+      <SelectMonedas />
+      <SelectCripto/>
         <Boton
             type='submit'
             value='Calcular'
